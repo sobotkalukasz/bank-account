8279f12320c13c15e6929dcaa0d3b0e986d7864c
sobotkalukasz <sobotka.lukasz@gmail.com>
2021-02-01 20:35:59 +0100

    initial commit

---
+++ src/test/java/pl/lsobotka/bank/account/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lsobotka.bank.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
