f9d17c651ade38d28d1cf7a45814adf856619b2a
Łukasz Sobótka <l.sobotka@firetms.com>
2023-04-04 20:50:05 +0200

    comment export java 15 in scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_15=/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home
-export JAVA_HOME=$JAVA_15
+#export JAVA_15=/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home
+#export JAVA_HOME=$JAVA_15
 
 mvn clean install
