1ec0e2211e66610110d0364ecaa5275e3265c889
Łukasz Sobótka <l.sobotka@firetms.com>
2023-04-04 20:37:30 +0200

    added embedded tomcat and build/run scripts

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export JAVA_15=/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home
+export JAVA_HOME=$JAVA_15
+
+java -jar --enable-preview target/bank-account-0.0.1-SNAPSHOT.jar
