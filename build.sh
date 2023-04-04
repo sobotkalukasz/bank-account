c190fe0ae70c09b60ebe541dbebd9988e199baee
Łukasz Sobótka <l.sobotka@firetms.com>
2023-04-04 21:14:55 +0200

    use java 17

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#export JAVA_15=/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home
-#export JAVA_HOME=$JAVA_15
+#export JAVA_17=/Library/Java/JavaVirtualMachines/jdk-17.0.2.jdk/Contents/Home
+#export JAVA_HOME=$JAVA_17
 
 mvn clean install
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#export JAVA_15=/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home
-#export JAVA_HOME=$JAVA_15
+#export JAVA_17=/Library/Java/JavaVirtualMachines/jdk-17.0.2.jdk/Contents/Home
+#export JAVA_HOME=$JAVA_17
 
 java -jar --enable-preview target/bank-account-0.0.1-SNAPSHOT.jar
